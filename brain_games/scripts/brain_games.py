9415e0e9ad17a2f4f4feae8fcd320ee725c0a251
Daniil Petrov <xjem666@yandex.ru>
2023-09-11 18:26:05 +0300

    step number 5(not end)

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+
+from random import randrange
 def main():
     print("Welcome to the Brain Games!")
     print('May I have your name? ', end='')
     name = input()
     print("Hello", name)
+
+def task():
+    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
+    number = randrange(100)
+    print("Question:", number)
+    answer_ = input() #Yes or No
+    print("Your answer:", answer_)
+    if answer_ = "Yes":
+        number % 2 = 0
