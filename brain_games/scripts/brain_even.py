3d3d007a4035bdf2c11c15057bc4ac29adb81020
Daniil Petrov <xjem666@yandex.ru>
2023-09-20 17:15:40 +0300

    step number 5(not end)

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,30 @@
+import random
+
+import prompt
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def is_even_str(num):
+    return "yes" if num % 2 == 0 else "no"
+
+
+
+def game():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(3):
+        random_num = random.randint(1, 11)
+        print(f'Question {random_num}')
+        user_answer = prompt.string('Your answer:')
+        correct_answer = "Yes" if is_even(random_num) else "No"
+        if user_answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"{correct_answer} is wrong answer ;(. Correct answer was {user_answer}")
+
+
+
+game()
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 
-from random import randrange
 def main():
-    print("Welcome to the Brain Games!")
-    print('May I have your name? ', end='')
+    print("Welcome to the brain games!")
+    print('May i have your name? ', end='')
     name = input()
     print("Hello", name)
 
-def task():
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
-    number = randrange(100)
-    print("Question:", number)
-    answer_ = input() #Yes or No
-    print("Your answer:", answer_)
-    if answer_ = "Yes":
-        number % 2 = 0
+
+main()
