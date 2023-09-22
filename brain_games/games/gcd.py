c7206f36a314d9b863bb8588f9738cc7a2a52e03
Daniil Petrov <xjem666@yandex.ru>
2023-09-23 01:12:47 +0300

    ("step number 7( finaly)")

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,20 @@
+import math
+import prompt
+from brain_games.utils import get_random_number
+
+
+def run_math_result():
+    for _ in range(3):
+        first_number, second_number = get_random_number(), get_random_number()
+        correct_answer = math.gcd(first_number, second_number)
+        print(f"Question {first_number}, {second_number}")
+        user_answer = prompt.string(f'Your answer - >')
+        if user_answer == str(correct_answer):
+            print("Correct")
+        else:
+            print(f'{user_answer} is wrong answer ;(. Correct answer was - > {correct_answer}')
+            return
+
+
+
+run_math_result()
