95d4fb8be2be793eee56c11aa215ce0083467cd8
Daniil Petrov <xjem666@yandex.ru>
2023-09-26 15:57:06 +0300

    step number 9

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,5 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string("Welcome to the Brain Games! \n May i Have your name? ")
+    name = prompt.string("Welcome to the Brain Games! \n May I Have your name? ")
     print(f"Hello, {name}")
