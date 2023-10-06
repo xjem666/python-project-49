9aaf68ae3b21e0a07ae1fb6c21025fc0b1de5bc4
Daniil Petrov <xjem666@yandex.ru>
2023-10-06 22:27:48 +0300

    remake(add command,asciinema)

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -16,5 +16,5 @@ def brain_gcd():
     return question, result
 
 
-def run_brain_nod():
+def run_brain_gcd():
     run_game(brain_gcd, NOD_INSTRUCTION)
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.games.nod import run_brain_nod
+from brain_games.games.gcd import run_brain_gcd
 
 
 def main():
-    run_brain_nod()
+    run_brain_gcd()
 
 
 if __name__ == "__main__":
