e4d7e56d5aabaf6bbcf3f535fb2172e4178836a2
Petrov Daniil <139789068+xjem666@users.noreply.github.com>
2023-11-28 10:09:19 +0300

    refactoring engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,16 +3,19 @@ from brain_games.const import AMOUNT_OF_ROUNDS
 
 
 def run_game(get_question_and_answer, instruction):
-    name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
-    print(f'Hello, {name}\n{instruction}')
+    name = prompt.string('Welcome to the Brain Games!\n'
+                         'May I have your name?')
+    print(f'Hello, {name}\n'
+          f'{instruction}')
     for _ in range(AMOUNT_OF_ROUNDS):
         question, correct_answer = get_question_and_answer()
-        user_answer = prompt.string(f'Question: {question}\nYour answer: ')
+        user_answer = prompt.string(f'Question: {question}\n'
+                                    f'Your answer: ')
         if correct_answer == user_answer:
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
+                  f"Correct answer was '{correct_answer}'."
+                  f"Let's try again, {name}!")
             return
     print(f"Congratulations, {name}!")
